c39224a4e0aad63f279d4185d33043eb39c530bb
libraroom <dickykutir1219@gmail.com>
2024-11-30 22:45:15 +0700

    viewkategori

--- routes/web.php
+++ routes/web.php
@@ -12,23 +12,21 @@ use App\Http\Controllers\DiskonController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\DetailPengadaanController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
 Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
 Route::post('/kategorii', [KategoriController::class, 'store'])->name('kategori.store');
 Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
+Route::post('/kategori/{kategori}', [KategoriController::class, 'update'])->name('kategori.update');
+Route::delete('/kategori/{kategori}', [KategoriController::class, 'delete'])->name('kategori.delete');
+Route::get('/kategori/{kategori}/edit', [KategoriController::class, 'edit'])->name('kategori.edit');
+
 Route::get('/stok', [BarangController::class, 'index'])->name('barang.index');
 Route::post('/barang', [BarangController::class, 'store'])->name('barang.store');
 Route::get('/barang/create', [BarangController::class, 'create'])->name('barang.create');
 Route::post('/barang/{barang}', [BarangController::class, 'update'])->name('barang.update');
 Route::delete('/barang/{barang}', [BarangController::class, 'delete'])->name('barang.delete');
 Route::get('/barang/{barang}/edit', [BarangController::class, 'edit'])->name('barang.edit');
-
-
-
 
 
 
