4cb1b4bf5295170b59d0e83a3e972d54a2289a80
JasmineAz <jasminezahraaa15@gmail.com>
2024-12-20 16:22:59 +0700

    api pajak

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/pajak', [\App\Http\Controllers\PajakController::class, 'index']);
