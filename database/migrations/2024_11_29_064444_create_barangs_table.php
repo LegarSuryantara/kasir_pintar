1adb16c9ef6e95ede6bc78ed55e5724e7c292032
Legar suryantara <legarsuryantara@gmail.com>
2024-12-25 21:46:18 +0700

    api_barang

--- database/migrations/2024_11_29_064444_create_barangs_table.php
+++ database/migrations/2024_11_29_064444_create_barangs_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nama_barang');
             $table->foreignId('kategori_id')->constrained();
+            $table->foreignId('toko_id')->constrained();
             $table->timestamps();
         });
     }
