5639f2f2127b40b9bf1add03e9c88aaf4d11eb7b
sherenaa <nissaserenaprimadani18@gmail.com>
2024-12-31 14:42:26 +0700

    push api diskon

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/diskon', [\App\Http\Controllers\DiskonController::class, 'index']);
 Route::get('/barang', [\App\Http\Controllers\BarangController::class, 'index']);
 Route::post('/barang', [\App\Http\Controllers\BarangController::class, 'store']);
 Route::get('/pajak', [\App\Http\Controllers\PajakController::class, 'index']);
@@ -21,3 +20,11 @@ Route::get('/toko', [\App\Http\Controllers\TokoController::class,'index']);
 Route::post('/toko', [\App\Http\Controllers\TokoController::class,'store']);
 Route::get('/supplier', [\App\Http\Controllers\SupplierController::class,'index']);
 Route::post('/supplier', [\App\Http\Controllers\SupplierController::class,'store']);
+
+
+//diskon
+Route::get('/diskon', [\App\Http\Controllers\DiskonController::class, 'indexApi']);
+Route::get('/diskon/{diskon}', [\App\Http\Controllers\DiskonController::class, 'getSingleData']);
+Route::post('/diskon', [\App\Http\Controllers\DiskonController::class, 'storeApi']);
+Route::put('/diskon/{diskon}', [\App\Http\Controllers\DiskonController::class, 'updateApi']);
+Route::delete('/diskon/{diskon}', [\App\Http\Controllers\DiskonController::class, 'deleteApi']);
